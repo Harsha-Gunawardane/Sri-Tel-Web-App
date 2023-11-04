507b57a683465dd711b0000b9927f04ae80dc8b8
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-11-04 22:06:07 +0530

    Implement some nessasary functionalities in service management

--- service-management/src/main/java/com/middleware/servicemanagement/repositories/UserActivatedServicesRepository.java
+++ service-management/src/main/java/com/middleware/servicemanagement/repositories/UserActivatedServicesRepository.java
@@ -11,4 +11,16 @@ public interface UserActivatedServicesRepository extends JpaRepository<UserActiv
     @Query(value = "SELECT u.serviceId FROM user_activated_services u " +
             "WHERE u.userId = ?1", nativeQuery = true)
     List<Integer> findAllServicesByUserId(Integer userId);
+
+    @Query(value = "SELECT u.serviceId FROM user_activated_services u " +
+            "WHERE u.userId = ?1 and u.serviceId = ?2", nativeQuery = true)
+    Integer findActivatedServiceByUserId(Integer userId, Integer serviceId);
+
+    @Query(value = "DELETE FROM user_activated_services u " +
+            "WHERE u.userId = ?1 and u.serviceId = ?2", nativeQuery = true)
+    Integer deleteActivatedServiceByUserId(Integer userId, Integer serviceId);
+
+    @Query(value = "DELETE FROM user_activated_services " +
+            "WHERE serviceId = ?1", nativeQuery = true)
+    void deleteAllByServiceId(Integer serviceId);
 }
